650b8a7fa56194843c92bfeee7c33952c4178184
yr53 <145339119+yr53@users.noreply.github.com>
2026-02-26 12:04:05 +0900

    chore: add Healthcontroller

---
+++ Coreon-Board-Service/src/main/java/com/coreon/board/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.coreon.board.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+  @GetMapping("/health")
+  public String health() {
+    return "OK";
+  }
+}
