7c0c924ff1474204249efdc33d1fbe1f702ebea2
yr53 <145339119+yr53@users.noreply.github.com>
2026-03-05 15:27:17 +0900

    update s3 credentials

--- Coreon_NoticeService/src/main/java/com/coreon/notice/config/AWSconfig.java
+++ Coreon_NoticeService/src/main/java/com/coreon/notice/config/AWSconfig.java
@@ -11,21 +11,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AWSconfig {
 
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
+    //@Value("${cloud.aws.credentials.access-key}")
+    //private String accessKey;
 
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;
+    //@Value("${cloud.aws.credentials.secret-key}")
+    //private String secretKey;
 
     @Value("${cloud.aws.region.static}")
     private String region;
 
     @Bean
     public AmazonS3 amazonS3() {
-        BasicAWSCredentials creds = new BasicAWSCredentials(accessKey, secretKey);
+        //BasicAWSCredentials creds = new BasicAWSCredentials(accessKey, secretKey);
         return AmazonS3ClientBuilder.standard()
                 .withRegion(region)
-                .withCredentials(new AWSStaticCredentialsProvider(creds))
+                //.withCredentials(new AWSStaticCredentialsProvider(creds))
                 .build();
     }
 }
